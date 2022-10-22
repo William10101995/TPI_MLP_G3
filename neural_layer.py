ecec95fcfdaeecdab786dc8408e074db511f3a64
KhalilKAN <kanasir@hotmail.com>
2022-10-22 17:23:18 -0300

    Error fixed, but no anda aun

--- neural_layer.py
+++ neural_layer.py
@@ -7,16 +7,10 @@ from IPython.display import clear_output
 from sklearn.datasets import make_circles
 
 
-dataset = create_dataset(1000, [pattern_B, pattern_D, pattern_F])
+dataset = create_dataset(1500, [pattern_B, pattern_D, pattern_F])
 
 input_X = np.array(dataset[0])
 input_Y = np.array(dataset[1])[:, np.newaxis]
-
-# plt.scatter(input_X[input_Y[:, 0] == 0, 0], input_X[input_Y[:, 0] == 0, 1], c="skyblue")
-# plt.scatter(input_X[input_Y[:, 0] == 1, 0], input_X[input_Y[:, 0] == 1, 1], c="salmon")
-# plt.axis("equal")
-# plt.show()
-
 
 p = 100
 # CLASE DE LA CAPA DE LA RED
@@ -45,7 +39,7 @@ def create_nn(topology, act_f):
     return nn
 
 
-topology = [p, 4, 8, 3]
+topology = [p, 5, 3]
 
 neural_net = create_nn(topology, sigm)
 
@@ -62,23 +56,11 @@ def train(neural_net, X, Y, l2_cost, lr=0.5, train=True):
 
     # Forward pass
     for l, layer in enumerate(neural_net):
-        print("out  ---> " + str(out[-1][1].shape))
-        # print(out[-1][1])
         
-
-        # print("a  ---> " + str(neural_net[l].act_f[0](out[-1][1] @ neural_net[l].W + neural_net[l].b).shape))
-        # print(neural_net[l].act_f[0](out[-1][1] @ neural_net[l].W + neural_net[l].b))
-        print("W    ---> " + str(neural_net[l].W.shape))
-        # print(neural_net[l].W)
-
-        # print("b    ---> " + str(neural_net[l].b.shape))
-        # print(neural_net[l].b)
-        # print()
         z = out[-1][1] @ neural_net[l].W + neural_net[l].b # Producto matricial con el @
         a = neural_net[l].act_f[0](z)
-        # print(a)
         out.append((z, a))
-    # print(l2_cost[0](out[-1][1],  Y))
+    #print(l2_cost[0](out[-1][1],  Y))
 
     if train:
         # Backward pass
@@ -104,43 +86,20 @@ def train(neural_net, X, Y, l2_cost, lr=0.5, train=True):
 
     return out[-1][1]
 
-# train(neural_net, input_X, input_Y, l2_cost, 0.5)
-
-
-
-
 
 neural_n = create_nn(topology, sigm)
 
 loss = [] 
-for i in range(1000):
+for i in range(4000):
   # Entrenamos a la red!
 
-  pY = train(neural_n, input_X, input_Y, l2_cost, lr=0.05)
+  pY = train(neural_n, input_X, input_Y, l2_cost, lr=0.01)
 
   if i % 25 == 0: # Cada 25 iteraciones, ejecuta lo de adentro
     loss.append(l2_cost[0](pY, input_Y))
-
-    res = 50
-
-    _x0 = np.linspace(-5, 5, res)
-    _x1 = np.linspace(-5, 5, res)
-
-    _Y = np.zeros((res, res))
-
-    for i0, x0 in enumerate(_x0):
-      for i1, x1 in enumerate(_x1):
-        _Y[i0, i1] = train(neural_n, np.array([[x0, x1]]), input_Y, l2_cost, train=False)[0][0]
-    
-    plt.pcolormesh(_x0, _x1, _Y, cmap="coolwarm")
-    plt.axis("equal")
-
-    plt.scatter(input_X[input_Y[:, 0] == 0, 0], input_X[input_Y[:, 0] == 0, 1], c="skyblue")
-    plt.scatter(input_X[input_Y[:, 0] == 1, 0], input_X[input_Y[:, 0] == 1, 1], c="salmon")
-
-    # clear_output(wait=True)
+    clear_output(wait=True)
+    plt.plot(range(len(loss)), loss)
     plt.show()
-    # plt.plot(range(len(loss)), loss)
-    # plt.show()
-    plt.close(1)
     time.sleep(0.5)
+
+print(loss)
