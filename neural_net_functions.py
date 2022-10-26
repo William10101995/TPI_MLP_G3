a809b3df11221d5e824cf6354d46e40213d56a33
KhalilKAN <kanasir@hotmail.com>
2022-10-25 21:04:41 -0300

    ultimos cambios... creo...

--- neural_net_functions.py
+++ neural_net_functions.py
@@ -3,25 +3,26 @@ from neural_net_components import forward_pass, back_propagation
 
 def which_letter_is(output, threshold):
     mapping = [
-        ("D", output[0][0]), 
+        ("B", output[0][0]), 
         ("F", output[0][1]), 
-        ("B", output[0][2])
+        ("D", output[0][2])
     ]
     
     letter_is = sorted(mapping, key=lambda x: x[1])
+    # print(letter_is)
     return [letter_is[2][0], letter_is[2][1]] if float(letter_is[2][1]) > threshold else "No se pudo identificar la letra"
 
 
-# defino una funcion para entrenar la red
+
 def train_once(neural_net, X, Y, lr, cost_f):
     outputs = forward_pass(neural_net, X)
     trained_neural_net = back_propagation(neural_net, Y, lr, cost_f, outputs)
-    # retorno la red neuronal entrenada
     return trained_neural_net
 
+# defino una funcion para entrenar la red
 def training(epochs, neural_net, X, Y, lr, cost_f):
     trained_neural_net = train_once(neural_net, X, Y, lr, cost_f)
-    for i in range(epochs):
+    for i in range(epochs - 1): # -1 porque lo entrena ya una vez al inicio de la funcion
         # Entrenamos a la red
         trained_neural_net = train_once(trained_neural_net, X, Y, lr, cost_f)
     return trained_neural_net
@@ -29,5 +30,5 @@ def training(epochs, neural_net, X, Y, lr, cost_f):
 # defino una funcion para predecir
 def predict(trained_neural_net, pattern):
     result = forward_pass(trained_neural_net, pattern)
-    
-    return which_letter_is(result[-1][1], 0.75)
+
+    return which_letter_is(result[-1][1], 0.1)
